0cf8e271a933c108d9df4677faa7aeeb6f1f81cf
Alex Schuy <alexjschuy@gmail.com>
2022-02-11 10:52:24 -0800

    Add mumu config.

---
+++ histos/mumu_vbs/rescale_int.C
@@ -0,0 +1,16 @@
+void rescale_int(const char *input, const char *output, float ratio)
+{
+    TFile *input_file = new TFile(input);
+    TFile *output_file = new TFile(output, "RECREATE");
+    TH1D *h, *h2;
+    TKey *key;
+    TIter next( input_file->GetListOfKeys());
+    int i = 0;
+    while ((key = (TKey *) next())) {
+        h = (TH1D *)input_file->Get(key->GetName()); // copy object to memory
+        h2 = (TH1D *)h->Clone();
+        h2->Scale(ratio);
+        h2->Write();
+    }
+
+}
